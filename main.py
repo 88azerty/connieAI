8d4daeb5657dc8e828bf1c68bd90e25419ae6195
Hernán Cabañas <hcabanasg@gmail.com>
2018-11-27 14:46:24 -0300

    ready for initial remote commit

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ Conectar ROOT con Tensorflow
 
 # from ROOT import TBranch, TTree, TFile, TChain, TBrowser
 # from ROOT import gROOT
-import uproot
-import math
+import uproot       # Se instala con pip install uproot [all]
 import pandas as pd
-import tensorflow as tf
-import numpy as np
+import tensorflow as tf # Se instala con pip install tensorflow[all]
 
-iterator = uproot.tree.iterate("../kcikel/10deg/*.root", "hitSumm", reportentries=False)
-o = 0
-
+iterator = uproot.tree.iterate("../kcikel/10deg/*.root", "hitSumm", reportentries=False) # hitSumm es el nombre del TTree dentro del TFile
 data = pd.DataFrame()
 
 for i in iterator:
     data = data.append(pd.DataFrame.from_dict(i), ignore_index=True)
     print("Leyendo datos")
+
 print(data.describe())
